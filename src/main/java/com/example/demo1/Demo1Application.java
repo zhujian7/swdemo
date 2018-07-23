fbb510c9ccfd7a4e3a9c9bd4a7e777180877c5d1
zhujian7 <zhujian@caicloud.io>
2018-07-23 19:26:20 +0800

    test:test

--- src/main/java/com/example/demo1/Demo1Application.java
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -3,11 +3,13 @@ package com.example.demo1;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
 public class Demo1Application {
     @Value("${test.url}")
     String url;
